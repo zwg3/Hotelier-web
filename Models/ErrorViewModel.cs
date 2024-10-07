efbc3d3768e1b6d044465c90a2c3dd2caf973fda
zwg3333 <60962298+zwg3333@users.noreply.github.com>
2024-10-07 10:25:52 +0300

    test commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Hotelier_web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
